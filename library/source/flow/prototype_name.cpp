90a5223abfcf83ab19d9e0eaeddd4b20d3c7228e
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-04-29 14:01:49 -0600

    Adds support for prototype_name concatenation & separator constant

--- library/source/flow/prototype_name.cpp
+++ library/source/flow/prototype_name.cpp
@@ -8,4 +8,13 @@ std::ostream& operator<<(std::ostream& os, const prototype_name& name)
     return os;
 }
 
+prototype_name operator+(const prototype_name& lhs, const prototype_name& rhs)
+{
+    auto result = std::string{};
+    result += lhs.value;
+    result += prototype_name::separator;
+    result += rhs.value;
+    return prototype_name{result};
+}
+
 }
