f382ea07544b4a22ab6fb505f01e35914783517a
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-04-29 14:04:18 -0600

    Adds assertion that underlying type for process_id should work for target OS

--- library/source/flow/process_id.cpp
+++ library/source/flow/process_id.cpp
@@ -1,6 +1,13 @@
+#include <type_traits>
+
+#include <unistd.h> // for pid_t
+
 #include "flow/process_id.hpp"
 
 namespace flow {
+
+// Ensure our enum's underlying type matches for the pid_t's actual type!
+static_assert(std::is_same_v<pid_t, std::underlying_type_t<process_id>>);
 
 std::ostream& operator<<(std::ostream& os, process_id value)
 {
