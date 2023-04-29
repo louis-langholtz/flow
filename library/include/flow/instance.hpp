62cd6b630e9a57caaa6150c8bf07809b0d83ee66
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-04-29 13:58:08 -0600

    Updates instance documentation

--- library/include/flow/instance.hpp
+++ library/include/flow/instance.hpp
@@ -14,9 +14,22 @@
 namespace flow {
 
 struct instance {
+
+    /// @brief Process ID.
+    /// @note This shall be <code>invalid_process_id</code> for default or failed instances,
+    /// <code>no_process_id</code> for system instances, less-than
+    /// <code>no_process_id</code> for the process group of the children, or
+    /// hold the POSIX process ID of the child process running for the instance.
     process_id id{invalid_process_id};
+
+    /// @brief Diagnostics stream.
+    /// @note This should be unique to this instance's process ID.
     ext::fstream diags;
+
+    /// @brief Sub-instances - or children - of this instance.
     std::map<prototype_name, instance> children;
+
+    /// @brief Channels made for this instance.
     std::vector<channel> channels;
 };
 
