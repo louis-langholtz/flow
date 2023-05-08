f2b75acb49de3159759a4bd7af4e26b6dc736d5f
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-05-07 18:14:07 -0600

    Adds dev_null file_endpoint constant for convenient reuse

--- library/source/flow/file_endpoint.cpp
+++ library/source/flow/file_endpoint.cpp
@@ -2,6 +2,8 @@
 
 namespace flow {
 
+const file_endpoint file_endpoint::dev_null = file_endpoint{"/dev/null"};
+
 auto operator<<(std::ostream& os, const file_endpoint& value) -> std::ostream&
 {
     os << "file_endpoint{";
