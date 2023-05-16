6d59e5f8418145e7e0007465ccbb506ab0a4ce2b
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-05-16 11:32:02 -0600

    Extends shell with libedit & more builtin command support

--- shell/main.cpp
+++ shell/main.cpp
@@ -1,10 +1,14 @@
+#include <cstdlib> // for std::strtol
 #include <functional> // for std::function
 #include <iomanip> // for std::quoted
 #include <iostream>
 #include <iterator>
+#include <memory> // for std::unique_ptr
 #include <ostream> // for std::flush
 #include <span>
 #include <string> // for std::getline
+
+#include <histedit.h>
 
 #include "flow/channel.hpp"
 #include "flow/connection.hpp"
@@ -16,9 +20,11 @@
 #include "flow/system_name.hpp"
 #include "flow/utility.hpp"
 
+namespace {
+
 template <class T, class U>
 auto operator==(const std::span<T>& lhs, const std::span<U>& rhs) ->
-    decltype(T{} == U{})
+decltype(T{} == U{})
 {
     if (lhs.size() != rhs.size()) {
         return false;
@@ -31,18 +37,59 @@ auto operator==(const std::span<T>& lhs, const std::span<U>& rhs) ->
     return true;
 }
 
+struct EditLineDeleter
+{
+    void operator()(EditLine *p)
+    {
+        el_end(p);
+    }
+};
+
+struct HistoryDeleter
+{
+    void operator()(History *p)
+    {
+        history_end(p);
+    }
+};
+
+char *prompt([[maybe_unused]] EditLine *el)
+{
+    static char buf[] = "\1\033[7m\1flow$\1\033[0m\1 ";
+    return buf;
+}
+
+}
+
 auto main(int argc, const char * argv[]) -> int
 {
     using arguments = std::vector<std::string>;
     using string_span = std::span<const std::string>;
     using cmd_handler = std::function<void(const string_span& args)>;
     using cmd_table = std::map<std::string, cmd_handler>;
+    using edit_line_ptr = std::unique_ptr<EditLine, EditLineDeleter>;
+    using history_ptr = std::unique_ptr<History, HistoryDeleter>;
 
     flow::environment_map environment = flow::get_environ();
     std::map<flow::system_name, flow::system> systems;
     std::map<flow::system_name, flow::instance> instances;
     flow::set_signal_handler(flow::signal::interrupt);
     flow::set_signal_handler(flow::signal::terminate);
+    auto do_loop = true;
+
+    // For example of using libedit, see: https://tinyurl.com/3ez9utzc
+    HistEvent ev{};
+    auto hist = history_ptr{history_init()};
+    history(hist.get(), &ev, H_SETSIZE, 100);
+
+    auto el = edit_line_ptr{el_init(argv[0], stdin, stdout, stderr)};
+    // el_parse(el.get(), argc, argv);
+
+    el_set(el.get(), EL_SIGNAL, 1); // installs sig handlers for resizing, etc.
+    el_set(el.get(), EL_HIST, history, hist.get());
+    el_set(el.get(), EL_PROMPT_ESC, prompt, '\1');
+
+    el_source(el.get(), NULL);
 
     // TODO: make this into a table of CRUD commands...
     const cmd_table cmds{
@@ -53,7 +100,7 @@ auto main(int argc, const char * argv[]) -> int
                     return;
                 }
             }
-            std::exit(0);
+            do_loop = false;
         }},
         {"help", [&](const string_span& args){
             for (auto&& arg: args) {
@@ -65,7 +112,26 @@ auto main(int argc, const char * argv[]) -> int
             std::cout << "Available commands:\n";
             for (auto&& entry: cmds) {
                 std::cout << entry.first << ": ";
-                (cmds.at(entry.first))(std::vector<std::string>{"--help"});
+                using strings = std::vector<std::string>;
+                const auto cargs = strings{entry.first, "--help"};
+                (cmds.at(entry.first))(cargs);
+            }
+        }},
+        {"history", [&](const string_span& args){
+            for (auto&& arg: args) {
+                if (arg == "--help") {
+                    std::cout << "shows the history of commands entered\n";
+                    return;
+                }
+                if (arg == "clear") {
+                    history(hist.get(), &ev, H_CLEAR);
+                    return;
+                }
+            }
+            for (auto rv = history(hist.get(), &ev, H_LAST);
+                 rv != -1;
+                 rv = history(hist.get(), &ev, H_PREV)) {
+                std::cout << ev.num << " " << ev.str;
             }
         }},
         {"show-env", [&](const string_span& args){
@@ -82,8 +148,13 @@ auto main(int argc, const char * argv[]) -> int
             auto usage = [&](std::ostream& os){
                 os << "usage: ";
                 os << args[0];
-                os << " <env-name> <env-value>\n";
+                os << " [--<flag>] [<env-name> <env-value>]\n";
+                os << "  where <floag> may be:\n";
+                os << "  --usage: shows this usage.\n";
+                os << "  --help: shows help on this command.\n";
+                os << "  --reset: resets the flow environment to given.\n";
             };
+            auto argc = 0;
             for (auto&& arg: args) {
                 if (arg == "--usage") {
                     usage(std::cout);
@@ -94,27 +165,39 @@ auto main(int argc, const char * argv[]) -> int
                     std::cout << "to the given value\n";
                     return;
                 }
-            }
-            if (args.size() < 2u) {
-                usage(std::cerr);
-                return;
-            }
-            environment[args[1]] = (args.size() > 2)? args[2]: "";
-        }},
-        {"unset-env", [&](const string_span& args){
-            for (auto&& arg: args) {
-                if (arg == "--help") {
-                    std::cout << "unsets the named environment variable\n";
-                    return;
+                if (arg == "--reset") {
+                    environment = flow::get_environ();
+                    --argc;
+                    continue;
                 }
             }
-            if (args.size() < 2u) {
-                std::cerr << "usage: ";
-                std::cerr << args[0];
-                std::cerr << " <env-name>\n";
-                return;
+            switch (argc) {
+            case 1:
+                break;
+            case 3:
+                environment[args[1]] = (args.size() > 2)? args[2]: "";
+                break;
+            default:
+                usage(std::cerr);
+                break;
             }
-            environment.erase(args[1]);
+        }},
+        {"unset-env", [&](const string_span& args){
+            for (auto&& arg: args.subspan(1u)) {
+                if (arg == "--help") {
+                    std::cout << "unsets the named environment variables\n";
+                    return;
+                }
+                if (arg == "--all") {
+                    environment.clear();
+                    continue;
+                }
+                if (environment.erase(arg) == 0) {
+                    std::cerr << "no such environment variable as ";
+                    std::cerr << std::quoted(arg);
+                    std::cerr << "\n" << std::flush;
+                }
+            }
         }},
         {"show-systems", [&](const string_span& args){
             for (auto&& arg: args) {
@@ -133,36 +216,95 @@ auto main(int argc, const char * argv[]) -> int
                 }
             }
         }},
-        {"add-system", [&](const string_span& args){
+        {"add-executable", [&](const string_span& args){
+            static const auto name_prefix = std::string{"--name="};
+            static const auto file_prefix = std::string{"--file="};
+            static const auto des_prefix = std::string{"--des-"};
+            static const auto in_str = std::string{"in"};
+            static const auto out_str = std::string{"out"};
             auto system = flow::system{};
+            system.info = flow::system::executable{};
+            auto& info = std::get<flow::system::executable>(system.info);
             auto name = flow::system_name{};
-            for (auto&& arg: args) {
+            auto index = 1u;
+            for (auto&& arg: args.subspan(1u)) {
+                ++index;
                 if (arg == "--help") {
-                    std::cout << "adds a new system definition.\n";
+                    std::cout << "adds a new executable system definition.\n";
+                    std::cout << "  usage: ";
+                    std::cout << args[0];
+                    std::cout << " --name=<name> --file=<file>";
+                    std::cout << " [--des-<n>=<in|out>[:<comment>]]";
+                    std::cout << " arg...\n";
                     return;
                 }
-                if (arg == "custom") {
-                    system.info = flow::system::custom{};
-                    continue;
-                }
-                if (arg == "executable") {
-                    system.info = flow::system::executable{};
-                    continue;
-                }
-                static const auto name_prefix = std::string{"name="};
                 if (arg.starts_with(name_prefix)) {
                     name = {arg.substr(name_prefix.length())};
                     continue;
                 }
+                if (arg.starts_with(file_prefix)) {
+                    info.file = {arg.substr(file_prefix.length())};
+                    continue;
+                }
+                if (arg.starts_with(des_prefix)) {
+                    auto des_id = flow::descriptor_id{};
+                    auto des_info = flow::descriptor_info{};
+                    const auto rem = arg.substr(des_prefix.size());
+                    char* p_end{};
+                    const auto n = std::strtol(rem.c_str(), &p_end, 10);
+                    if ((p_end == rem.c_str()) || (*p_end != '=')) {
+                        std::cerr << "aborting: unrecognized argument ";
+                        std::cerr << std::quoted(arg);
+                        std::cerr << "\n";
+                        return;
+                    }
+                    des_id = flow::descriptor_id(n);
+                    ++p_end; // skip over '='
+                    if (in_str.compare(0, in_str.size(),
+                                       p_end, in_str.size()) == 0) {
+                        des_info.direction = flow::io_type::in;
+                        p_end += in_str.size();
+                    }
+                    else if (out_str.compare(0, out_str.size(),
+                                             p_end, out_str.size()) == 0) {
+                        des_info.direction = flow::io_type::out;
+                        p_end += out_str.size();
+                    }
+                    else {
+                        std::cerr << "aborting: unrecognized direction in ";
+                        std::cerr << std::quoted(arg);
+                        std::cerr << "\n";
+                        return;
+                    }
+                    if ((*p_end != '\0') && (*p_end != ':')) {
+                        std::cerr << "aborting: unrecognized ending of ";
+                        std::cerr << std::quoted(arg);
+                        std::cerr << " seeing '";
+                        std::cerr << *p_end;
+                        std::cerr << "'\n";
+                        return;
+                    }
+                    if (*p_end == ':') {
+                        ++p_end;
+                        des_info.comment = std::string{p_end};
+                    }
+                    system.descriptors[des_id] = des_info;
+                    continue;
+                }
+                --index;
+                break;
             }
+            const auto arg_span = args.subspan(index);
+            info.arguments = std::vector(begin(arg_span), end(arg_span));
             systems.emplace(name, system);
         }},
     };
 
-    auto prompt = std::string{"> "};
-    std::cout << prompt << std::flush;
-    std::string line;
-    while (std::getline(std::cin, line)) {
+    auto count = 0;
+    auto buf = static_cast<const char*>(nullptr);
+    while (do_loop && ((buf = el_gets(el.get(), &count)) != nullptr)) {
+        history(hist.get(), &ev, H_ENTER, buf);
+        auto line = std::string{buf, buf + count};
         std::string command;
         arguments args;
         std::string word;
@@ -180,8 +322,6 @@ auto main(int argc, const char * argv[]) -> int
             std::cerr << "no such command as " << std::quoted(command) << ".\n";
             std::cerr << "enter " << std::quoted("help") << " for help.\n";
         }
-        std::cout << prompt << std::flush;
     }
-
     return 0;
 }
