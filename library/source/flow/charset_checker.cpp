d9f9d7b1d3b0607df3625b36d6673a6208fc3c5f
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-05-17 19:12:24 -0600

    Refactors reserved_chars_checker into more general charset_checker

--- library/source/flow/charset_checker.cpp
+++ library/source/flow/charset_checker.cpp
@@ -1,16 +1,19 @@
 #include <stdexcept> // for std::invalid_argument
 #include <sstream> // for std::ostringstream
 
-#include "flow/reserved_chars_checker.hpp"
+#include "flow/charset_checker.hpp"
 
 namespace flow::detail {
 
-auto reserved_chars_validator(std::string v,
-                              const std::string& reserved_chars)
+auto charset_validator(std::string v,
+                       char_list access,
+                       const std::string& chars)
     -> std::string
 {
-    if (const auto found = v.find_first_of(reserved_chars);
-        found != std::string::npos) {
+    const auto found = (access == char_list::deny)
+        ? v.find_first_of(chars)
+        : v.find_first_not_of(chars);
+    if (found != std::string::npos) {
         const auto c = v[found];
         std::ostringstream os;
         os << "may not contain '";
