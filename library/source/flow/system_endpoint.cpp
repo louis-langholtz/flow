501726c7364a8394e6f789ee13896b43e0c31c21
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-05-19 19:28:14 -0600

    Adds some compile time validation of separator used for output streaming system_endpoint

--- library/source/flow/system_endpoint.cpp
+++ library/source/flow/system_endpoint.cpp
@@ -1,12 +1,20 @@
+#include <algorithm>
+
 #include "flow/system_endpoint.hpp"
 
 namespace flow {
 
 auto operator<<(std::ostream& os, const system_endpoint& value) -> std::ostream&
 {
+    using std::begin, std::end;
+    constexpr auto separator = ':';
+    // ensure separator not valid char for address
+    static_assert(std::count(begin(detail::name_charset{}),
+                             end(detail::name_charset{}),
+                             separator) == 0);
     os << "system_endpoint{";
     os << value.address.get();
-    os << ':';
+    os << separator;
     auto prefix = "";
     for (auto&& descriptor: value.descriptors) {
         os << prefix << descriptor;
