85cba8b9bcf72de1f9367db416a30d5537283ce7
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-06-17 08:21:52 -0600

    Refactors make_channel & supporting functions to pass more specificly needed data

--- tests/channel.cpp
+++ tests/channel.cpp
@@ -16,7 +16,7 @@ TEST(channel, default_construction)
 TEST(make_channel, with_defaulted_args)
 {
     using flow::link; // disambiguate link
-    EXPECT_THROW(make_channel(link{}, node_name{}, flow::node{}, {},
+    EXPECT_THROW(make_channel(link{}, node_name{}, port_map{}, {}, {},
                               {}, {}), invalid_link);
 }
 
@@ -28,7 +28,7 @@ TEST(make_channel, with_diff_sizes)
             {}, {reference_descriptor{1}}
         }}
     };
-    EXPECT_THROW(make_channel(link{}, node_name{}, flow::node{}, {},
+    EXPECT_THROW(make_channel(link{}, node_name{}, port_map{}, {}, {},
                               pconns, {}), std::logic_error);
 }
 
@@ -37,7 +37,7 @@ TEST(make_channel, same_ends)
     const auto the_end = node_endpoint{"a"};
     try {
         make_channel(unidirectional_link{the_end, the_end}, node_name{},
-                     flow::node{}, {}, {}, {});
+                     port_map{}, {}, {}, {}, {});
         FAIL() << "expected invalid_link, got none";
     }
     catch (const invalid_link& ex) {
@@ -56,7 +56,7 @@ TEST(make_channel, end_to_nonesuch)
         node_endpoint{node_name{"b"}},
     }};
     try {
-        make_channel(the_link, node_name{}, flow::node{}, {}, {}, {});
+        make_channel(the_link, node_name{}, port_map{}, {}, {}, {}, {});
         FAIL() << "expected invalid_link, got none";
     }
     catch (const invalid_link& ex) {
@@ -84,7 +84,8 @@ TEST(make_channel, for_subsys_to_file)
     };
     const auto pconns = std::vector<link>{};
     auto pchans = std::vector<channel>{};
-    EXPECT_NO_THROW(chan = make_channel(conn, name, sys, {}, pconns, pchans));
+    EXPECT_NO_THROW(chan = make_channel(conn, name, port_map{}, sys, {},
+                                        pconns, pchans));
     EXPECT_TRUE(std::holds_alternative<file_channel>(chan));
 }
 
@@ -104,7 +105,8 @@ TEST(make_channel, for_file_to_subsys)
     };
     const auto pconns = std::vector<link>{};
     auto pchans = std::vector<channel>{};
-    EXPECT_NO_THROW(chan = make_channel(conn, name, sys, {}, pconns, pchans));
+    EXPECT_NO_THROW(chan = make_channel(conn, name, port_map{}, sys, {},
+                                        pconns, pchans));
     EXPECT_TRUE(std::holds_alternative<file_channel>(chan));
 }
 
@@ -125,7 +127,8 @@ TEST(make_channel, for_default_subsys_to_default_subsys)
     };
     const auto pconns = std::vector<link>{};
     auto pchans = std::vector<channel>{};
-    EXPECT_NO_THROW(chan = make_channel(conn, name, sys, {}, pconns, pchans));
+    EXPECT_NO_THROW(chan = make_channel(conn, name, port_map{}, sys, {},
+                                        pconns, pchans));
     EXPECT_TRUE(std::holds_alternative<pipe_channel>(chan));
 }
 
@@ -134,14 +137,12 @@ TEST(make_channel, for_exe_subsys_to_sys)
     using flow::link; // disambiguate link
     auto chan = channel{};
     const auto name = node_name{};
-    const auto sys = flow::node{
-        flow::system{
-            .nodes = {
-                {"subsys_a", flow::executable{}},
-            }
-        }, std_ports,
+    const auto sys = flow::system{
+        .nodes = {
+            {"subsys_a", flow::executable{}},
+        }
     };
-    ASSERT_FALSE(empty(sys.interface));
+    ASSERT_FALSE(empty(std_ports));
     const auto conn = unidirectional_link{
         node_endpoint{"subsys_a", {reference_descriptor{1}}},
         node_endpoint{{}, {reference_descriptor{1}}},
@@ -153,7 +154,8 @@ TEST(make_channel, for_exe_subsys_to_sys)
     };
     auto pchans = std::vector<channel>{};
     pchans.emplace_back(channel{pipe_channel{}});
-    EXPECT_NO_THROW(chan = make_channel(conn, name, sys, {}, pconns, pchans));
+    EXPECT_NO_THROW(chan = make_channel(conn, name, std_ports, sys, {},
+                                        pconns, pchans));
     EXPECT_TRUE(std::holds_alternative<reference_channel>(chan));
 }
 
@@ -163,19 +165,17 @@ TEST(make_channel, signal_channel)
     const auto exe_name = node_name{"exe"};
     const auto sig = flow::signals::winch();
     auto name = flow::node_name{};
-    auto sys = flow::node{
-        flow::system{
-            .nodes = {{exe_name, {
-                executable{},
-                port_map{
-                    {sig, {"", io_type::in}},
-                    stdout_ports_entry,
-                    stderr_ports_entry
-                }
-            }}}
-        },
-        port_map{{sig, {"", io_type::in}}}
+    auto sys = flow::system{
+        .nodes = {{exe_name, {
+            executable{},
+            port_map{
+                {sig, {"", io_type::in}},
+                stdout_ports_entry,
+                stderr_ports_entry
+            }
+        }}}
     };
+    const auto interface = port_map{{sig, {"", io_type::in}}};
     auto conn = flow::unidirectional_link{
         node_endpoint{{}, {sig}},
         node_endpoint{exe_name, {sig}},
@@ -183,7 +183,8 @@ TEST(make_channel, signal_channel)
     auto channels = std::vector<channel>{};
     auto pconns = std::vector<link>{};
     auto pchans = std::vector<channel>{};
-    const auto rv = make_channel(conn, name, sys, channels, pconns, pchans);
+    const auto rv = make_channel(conn, name, interface, sys, channels,
+                                 pconns, pchans);
     ASSERT_TRUE(std::holds_alternative<signal_channel>(rv));
     const auto& sc = std::get<signal_channel>(rv);
     EXPECT_EQ(sc.address, exe_name);
